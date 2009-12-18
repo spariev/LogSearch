9ff44973f39a283e2f381ee7bc59bd2572281e25
spariev <spariev@gmail.com>
2009-12-18 09:18:54 +0300

    parsing log files, take 1

--- src/com/spariev/parse/file.clj
+++ src/com/spariev/parse/file.clj
@@ -1,7 +1,15 @@
 (ns com.spariev.parse.file
  (:use [clojure.contrib.duck-streams :only [reader]]
-       [clojure.contrib.seq-utils :only [partition-all]]))
+   [clojure.contrib.seq-utils :only [partition-by]]))
+
+(defn process-request-log [log-chunk]
+  (println "Processing another chunk ..."))
 
 (defn parse-file [fname]
   (let [req-counter (ref 0)]
-  (partition-by #( ) (line-seq (reader fname)))
+    (doall (map process-request-log
+       (partition-by
+         #((if (. % startsWith "Processing")
+             (dosync (alter req-counter inc))
+             (deref req-counter)))
+         (line-seq (reader fname)))))))
