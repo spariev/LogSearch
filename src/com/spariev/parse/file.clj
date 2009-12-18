9f96961afc76c354cf4bbaa721f1e44ff0f6cc5c
unknown <spariev@.(none)>
2009-12-18 19:14:31 +0300

    saving parsed logs in mongodb

--- src/com/spariev/parse/file.clj
+++ src/com/spariev/parse/file.clj
@@ -1,15 +1,17 @@
 (ns com.spariev.parse.file
  (:use [clojure.contrib.duck-streams :only [reader]]
+   com.spariev.db
    [clojure.contrib.seq-utils :only [partition-by]]))
 
-(defn process-request-log [log-chunk]
-  (println "Processing another chunk ..."))
+(defn process-request-log [req-id log-chunk]
+  (save-req (deref req-id) log-chunk)
+  [])
 
 (defn parse-file [fname]
   (let [req-counter (ref 0)]
-    (doall (map process-request-log
+    (doall (map (partial process-request-log req-counter)
        (partition-by
-         #((if (. % startsWith "Processing")
+         #(if (. % startsWith "Processing")
              (dosync (alter req-counter inc))
-             (deref req-counter)))
+             (deref req-counter))
          (line-seq (reader fname)))))))
