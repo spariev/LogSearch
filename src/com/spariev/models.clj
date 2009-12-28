cbb73fb31620c3846edb0c2f3d8088d4a1373900
spariev <spariev@gmail.com>
2009-12-28 09:03:08 +0300

    refactored parsers

--- src/com/spariev/models.clj
+++ src/com/spariev/models.clj
@@ -1,11 +1,19 @@
 (ns com.spariev.models
   ;(:require )
-  ;(:use )
+  (:use com.spariev.parse.rails)
   ;(:import )
   )
 
 (defstruct rails-req
   :start-time :controller :method :remote-addr :format :http-method ; data from header (Processing ...)
-  :duration :view-duration :db-duration :http-code :url) ; data from last request line (Completed ...)
+  :duration :view-duration :db-duration :http-code :url
+  :body) ; data from last request line (Completed ...)
+
+(defn create-rails-req [log-chunk]
+  (let [proc-line (first log-chunk)
+        proc-line-data (parse-processed-line proc-line)
+        comp-line (first (filter #( >= (.indexOf % "Completed on") 0) (rest log-chunk)))
+        comp-line-data (if comp-line (parse-completed-line comp-line) {})]
+    (merge proc-line-data comp-line-data)))
 
 
--- src/com/spariev/parse/rails.clj
+++ src/com/spariev/parse/rails.clj
@@ -1,78 +1,59 @@
 
 (ns com.spariev.parse.rails
-;  (:require  )
   (:use com.spariev.parse.common name.choi.joshua.fnparse clojure.contrib.error-kit
-        [clojure.contrib.seq-utils :only [flatten]])
-  ;(:import )
-  )
+        [clojure.contrib.seq-utils :only [flatten]]))
 
 (defn to-str [a-seq]
-  (reduce str(flatten a-seq)))
+  (reduce str "" (flatten a-seq)))
 
-(def test-string "Processing EpisodesController#first_unapproved to xml (for 192.168.212.244 at 2009-12-10 04:59:43) [GET]")
+(def processing-test-string "Processing EpisodesController#first_unapproved to xml (for 192.168.212.244 at 2009-12-10 04:59:43) [GET]")
+(def completed-test-string
+  "18112: 2009-12-10 04:59:43.108943 | Completed in 405ms (View: 1, DB: 15) | 422 Unprocessable Entity [http://admin.telemarker.home/episodes/first_unapproved.xml?lang=ru] ")
 
 (def minus-lit (lit \-))
 (def colon-lit (lit \:))
+(defn word-lit [wrd]
+  (lit-conc-seq (seq wrd)))
 
-(def controller-method-delimiter (lit \#))
+(def space-lit (lit \space))
+(def tab-lit (lit \tab))
+(def ignored-words-lit (alt (word-lit "to") (word-lit "for") (word-lit "at")))
 
-(def controller-and-method
-  (complex [controller-name (rep+ (except anything controller-method-delimiter))
-            _     controller-method-delimiter
-            method-name (rep+ (except anything (lit \space)))]
-    (vec (map apply-str [controller-name method-name]))))
-  
-(def ip-addr
-  (complex [ip (conc
-               (rep+ decimal-digit) dec-point
-               (rep+ decimal-digit) dec-point
-               (rep+ decimal-digit) dec-point
-               (rep+ decimal-digit))]
-    (reduce str (flatten ip))))
+(def ws (constant-semantics (rep* (alt ignored-words-lit (lit \|) (lit \() (lit \)) space-lit tab-lit )) :ws))
 
-(def datetime
-  (complex [datetime-parsed (conc
-                (rep+ decimal-digit) minus-lit
-                (rep+ decimal-digit) minus-lit
-                (rep+ decimal-digit) space-lit
-                (rep+ decimal-digit) colon-lit
-                (rep+ decimal-digit) colon-lit
-                (rep+ decimal-digit))]
-                (to-str datetime-parsed)))
-
-(def completed-string
-  "18112: 2009-12-10 04:59:43.108943 | Completed in 405ms (View: 1, DB: 15) | 422 Unprocessable Entity [http://admin.telemarker.home/episodes/first_unapproved.xml?lang=ru] ")
-
-;(def completed-line-rule
-;  (conc process-id line-datetime ws completed ws duration ws view-duration
-;    db-duration ws http-code ws result-format ws ip-addr ws datetime ws http-method))
-
-(defstruct state-s :remainder)
-
-(def remainder-a
-  (accessor state-s :remainder))
-
-
-(defn inv-doc-fn [state]
-  (println "invalid document \"%s\""   (apply-str (remainder-a state))))
-
-(defn data-left-fn [data-left state]
-  (println "leftover data after a valid node \"%s\"" (apply-str (remainder-a state))))
-
-(def processing-line-rule
-  (conc processing ws controller-and-method ws result-format ws ip-addr ws datetime ws http-method))
+(def dec-point (lit \.))
+(def decimal-digit (lit-alt-seq "0123456789"))
 
 (def digits
   (rep+ decimal-digit))
+  
+(def ip-addr-lit
+  (conc digits dec-point digits dec-point digits dec-point digits))
+
 (def datetime-lit
   (conc
-                (rep+ decimal-digit) minus-lit
-                (rep+ decimal-digit) minus-lit
-                (rep+ decimal-digit) space-lit
-                (rep+ decimal-digit) colon-lit
-                (rep+ decimal-digit) colon-lit
-                (rep+ decimal-digit)))
+    digits minus-lit digits minus-lit digits
+    space-lit
+    digits colon-lit digits colon-lit digits))
 
+(def processing-rule-complex
+  (complex [ _               (word-lit "Processing")
+             _               ws
+             controller-name (rep+ (except anything (lit \#)))
+             _               (lit \#)
+             method-name     (rep+ (except anything space-lit))
+             _               ws
+             result-format   (alt (word-lit "xml") (word-lit "html") (word-lit "xls"))
+             _               ws
+             ip-addr         ip-addr-lit
+             _               ws
+             req-time        datetime-lit
+             _               ws
+             _               (lit \[)
+             http-method     (alt (word-lit "GET") (word-lit "POST") (word-lit "PUT") (word-lit "DELETE"))
+             _               (lit \])]
+    {:start-time (to-str req-time) :controller (to-str controller-name) :method (to-str method-name)
+     :remote-addr (to-str ip-addr) :format (to-str result-format) :http-method (to-str http-method)})); TODO come up with more clever way to do it
 
 (def completed-rule-complex
   (complex [ process-id digits
@@ -103,8 +84,26 @@
              _           (lit \])
              _           ws
              ]
-    { :process-id process-id :end-time datetime :duration duration
-      :view-duration view-dur :db-duration db-dur :http-code http-code :url full-url }))
+    { :process-id (to-str process-id) :end-time (to-str datetime) :duration (to-str duration)
+      :view-duration (to-str view-dur) :db-duration (to-str db-dur)
+      :http-code (to-str http-code) :url (to-str full-url) }))
 
-(defn parse-line [line-str line-rule]
+(defstruct state-s :remainder)
+(def remainder-a
+  (accessor state-s :remainder))
+
+
+(defn inv-doc-fn [state]
+  (println "invalid document \"%s\""   (to-str (remainder-a state))))
+
+(defn data-left-fn [data-left state]
+  (println "leftover data after a valid node \"%s\"" (to-str (remainder-a state))))
+
+(defn parse-line [line-rule line-str]
     (rule-match line-rule inv-doc-fn data-left-fn (struct state-s (seq line-str))))
+
+(defn parse-processed-line
+  (partial parse-line processed-rule-complex))
+
+(defn parse-completed-line
+  (partial parse-line completed-rule-complex))
