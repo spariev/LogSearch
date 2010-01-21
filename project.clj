4c8f44af9e82c7ae0473457880471135fab2fded
Sergey Pariev <spariev@gmail.com>
2010-01-21 19:27:35 +0300

    moving to lein

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject LogSearch "0.1.0"
+	  :description "LogSearch"
+	  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+			 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+			 [org.apache.lucene/lucene-core "3.0.0"]
+			 [org.apache.lucene/lucene-highlighter "3.0.0"]
+			 [org.clojars.somnium/congomongo "0.1.1-SNAPSHOT"]
+			 ]
+	  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+	  :main clj_renderer)
