20531632f3fbd800b695bc836c2dcc197a0f6ef8
Sergey Pariev <spariev@gmail.com>
2010-01-27 12:23:11 +0300

    switching to purcell's congomongo fork

--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
 			 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
 			 [org.apache.lucene/lucene-core "3.0.0"]
 			 [compojure "0.3.2"]
+			 [joda-time/joda-time "1.6"]
 			 [org.apache.lucene/lucene-highlighter "3.0.0"]
-			 [org.clojars.somnium/congomongo "0.1.1-SNAPSHOT"]
+			 [org.clojars.sanityinc/congomongo "0.1.1-SNAPSHOT"]
+			 #_[org.clojars.somnium/congomongo "0.1.1-SNAPSHOT"]
+			 [spariev/chrono "1.0.0-SNAPSHOT"]
 			 ]
 	  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
 	  :main clj_renderer)
