d3ceed5c96599073d4057c9007bd6f1de34128b6
spariev <spariev@gmail.com>
2010-02-12 15:41:59 +0300

    parsing lines with regexps, many indexing improvement and refactorings

--- src/spariev/logsearch/fileparser.clj
+++ src/spariev/logsearch/fileparser.clj
@@ -1,6 +1,7 @@
 (ns spariev.logsearch.fileparser
- (:use [clojure.contrib.duck-streams :only [reader]]
-       [clojure.contrib.str-utils2 :as strutils :only [join]]
+  (:refer-clojure :exclude [contains?])
+  (:use [clojure.contrib.duck-streams :only [reader]]
+       [clojure.contrib.str-utils2 :as strutils :only [join blank?]]
        [spariev.chrono :as chrono]
        [spariev.logsearch.db :as db]
        spariev.config
@@ -10,59 +11,86 @@
        [clojure.contrib.seq-utils :only [partition-by]])
  (:import (org.apache.lucene.document CompressionTools)))
 
-(defn parse-and-save-req
-  [req-lines]
-  (let [gridfs-file (db/pack-into-file :logsfs (rest req-lines))]
-    (insert! :railslogs
-	     {:hdr (first req-lines)
-	      :parsed-hdr (parse-line-regexp processing-rule-regexp (first req-lines))
+#_(set! *warn-on-reflection* true)
+
+(defn parse-log-chunk
+  [chunk]
+  (do
+    #_(println "=== ")
+    (let [hdr (first chunk)
+	parsed-hdr (parse-processing-line (first chunk))
+	cntr (ref 0)
+	content (->> (partition-by #(if (ordinary-line? %)
+				      (dosync (alter cntr inc))
+				      (deref cntr))
+				   (rest chunk))
+		     (map #(parse-ordinary-line (strutils/join " " %)))
+		     (map #(% :content))
+		     (filter #(not (strutils/blank? %))))
+        parsed-ftr (let [complete-lines (map parse-completed-line (filter #(completed-line? %) content))]
+		     (if (> (count complete-lines) 0)
+		       (first complete-lines) ; there could be only one completed line anyway
+		       {}))]
+    {:hdr hdr
+     :attrs (into parsed-hdr parsed-ftr)
+     :content content })))
+
+(defn save-req
+  [req-lines parsed-chunk config-id]
+  (let [gridfs-file (db/pack-into-file (fs-name-for-config config-id) (rest req-lines))
+	{:keys [hdr attrs]} parsed-chunk]
+    (insert! (db-name-for-config config-id)
+	     {:hdr hdr
+	      :attr attrs
 	      :file_id (str (gridfs-file :_id))})))
 
-(defn process-request-log [index-writer log-chunk]
-  (do
-    #_(println (first log-chunk))
-    (let [mongo-log (parse-and-save-req log-chunk)
-	  lucene-doc (parse-log-chunk (.toString (mongo-log :_id)) log-chunk)]
-      (.addDocument index-writer lucene-doc)
+(defn process-request-log [config-id index-writer log-chunk]
+  (if (and (> (count log-chunk) 1) (processing-line? (first log-chunk)))
+    (let [parsed-chunk (parse-log-chunk log-chunk)
+	  mongo-log (save-req log-chunk parsed-chunk config-id)
+	  lucene-doc (create-doc-from-chunk (.toString #^com.mongodb.ObjectId (mongo-log :_id)) parsed-chunk)]
+      (.addDocument #^org.apache.lucene.index.IndexWriter index-writer lucene-doc)
       nil)))
 
-(defn index-file-with-writer [index-writer fname]
+(defn index-file-with-writer [config-id index-writer fname]
   (let [req-counter (ref 0)]
     (do
       (println (str "Parsing " fname))
-      (doall (map (partial process-request-log index-writer)
+      (doall (map (partial process-request-log config-id index-writer)
 		(partition-by
-		 #(if (. % startsWith "Processing")
+		 #(if (. #^String % startsWith "Processing")
 		    (dosync (alter req-counter inc))
 		    (deref req-counter))
 		 (line-seq (reader fname)))))
 		'())))
 
-(defn index-file [filename db-name idx-name]
-  (let [idx-dir (index-dir idx-name) ]
+(defn index-file [filename config-id]
+  (let [idx-dir (index-dir (idx-path-for-config config-id))]
     (do
       (org.apache.lucene.index.IndexWriter/unlock idx-dir)
-      (somnium.congomongo/mongo! :db db-name)
+      (somnium.congomongo/mongo! :db (db-name-for-config config-id))
       (with-open [#^org.apache.lucene.index.IndexWriter writer (prepare-lucene-index idx-dir)]
 	(do
-	  (index-file-with-writer writer filename)
+	  (index-file-with-writer config-id writer filename)
 	  (println "Indexing is over"))))))
 
 
-(defn index-app-logs [date config-id]
+(defn index-app-logs [config-id date]
   (let [path (get-config-val config-id :path)
-	dirfile (java.io.File. path)
+	dirfile (java.io.File. #^String path)
 	formatted-date (chrono/format-date date :compact-date)
-	fnames (map #(str path path-sep % ) (filter #(> (.indexOf % formatted-date) 0)
-		       (if dirfile (.list dirfile) [])))
+	fnames (sort (map #(str path path-sep % )
+		    (filter #(> (.indexOf #^String % #^String formatted-date) 0)
+		       (if dirfile (.list dirfile) []))))
 	idx-dir-fname (idx-path-for-config config-id)
 	_ (println (str "=== indexing to " idx-dir-fname))
+	_ (println fnames)
 	idx-dir (index-dir idx-dir-fname) ]
     (do
       (org.apache.lucene.index.IndexWriter/unlock idx-dir)
       (somnium.congomongo/mongo! :db (db-name-for-config config-id))
       (with-open [#^org.apache.lucene.index.IndexWriter writer (prepare-lucene-index idx-dir)]
-	(doall (pmap (partial index-file-with-writer writer) fnames))
+	(doall (pmap (partial index-file-with-writer config-id writer) fnames))
 	(println "Indexing is over")))))
 
 
--- src/spariev/logsearch/parser.clj
+++ src/spariev/logsearch/parser.clj
@@ -1,18 +1,63 @@
-(ns spariev.logsearch.parser)
+(ns spariev.logsearch.parser
+  (:refer-clojure :exclude [contains?])
+  (:use [clojure.contrib.str-utils2 :as strutils :only [contains?]]))
+
+#_(set! *warn-on-reflection* true)
 
 (def processing-test-string "Processing EpisodesController#first_unapproved to xml (for 192.168.212.244 at 2009-12-10 04:59:43) [GET]")
 (def completed-test-string
   "18112: 2009-12-10 04:59:43.108943 | Completed in 405ms (View: 1, DB: 15) | 422 Unprocessable Entity [http://admin.telemarker.home/episodes/first_unapproved.xml?lang=ru] ")
 
-(def processing-rule-regexp
+(def processing-line-check
+     "Processing ")
+(def processing-line-regexp
      #"Processing ((?:\w+::)*\w+)#(\w+)(?: to (\w+))? \(for (\d+\.\d+\.\d+\.\d+) at (\d+-\d+-\d+ \d+:\d+:\d+)\) \[(.*)\]")
 
+(def completed-line-check
+     "Completed in ")
+(def completed-line-regexp
+     #"Completed in (\d+)ms \((?:View: (\d+), )?DB: (\d+)\) \| (\d\d\d).+\[(http.+)\]")
+
+(def ordinary-line-regexp
+     #"(\d+): (\d+-\d+-\d+ \d+:\d+:\d+)\.\d\d\d\d\d\d \|(.*)")
+
+(def ordinary-line-check
+     #"(\d+): (\d+-\d+-\d+ \d+:\d+:\d+)\.\d\d\d\d\d\d \|(.*)")
+
+(defn processing-line?
+  [line]
+  (strutils/contains? line processing-line-check))
+
+(defn completed-line?
+  [line]
+  (strutils/contains? line completed-line-check))
+
+(defn ordinary-line?
+  [line]
+  (re-matches ordinary-line-check line))
+
 (defn parse-line-regexp
-  [re line]
-  (let [matches (first (re-seq re line))
-	r-keys [:controller-name :method-name :result-format :ip-addr :req-time :http-method]]
-    (if (= 7 (count matches))
-      (zipmap r-keys (rest matches))
-      (do
-	(println (str "Invalid line " line))
-	{}))))
+  [line re r-keys]
+  (let [matches (first (re-seq re line))]
+    (do
+      #_(println (rest matches))
+      #_(println (count (rest matches)))
+      (if (= (count r-keys) (count (rest matches)))
+	(zipmap r-keys (rest matches))
+	(do
+	  (println (str "Invalid line " line))
+	  {})))))
+
+(defn parse-processing-line
+  [line]
+  (parse-line-regexp line processing-line-regexp
+		     [:controller-name :method-name :result-format :ip-addr :req-time :http-method]))
+
+(defn parse-completed-line
+  [line]
+  (parse-line-regexp line completed-line-regexp [:duration :view :db :status :url]))
+
+(defn parse-ordinary-line
+  [line]
+  (parse-line-regexp line ordinary-line-regexp [:thread :datetime :content]))
+
